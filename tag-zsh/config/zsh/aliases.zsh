d62391042b43c0bed00a50e5ff70963ea79c10c4
John Watson <john@photodojo.org>
2017-09-01 14:52:59 -0400

    Minor shell tweaks.

--- tag-zsh/config/zsh/aliases.zsh
+++ tag-zsh/config/zsh/aliases.zsh
@@ -5,12 +5,9 @@ alias gpd='git config user.email "john@photodojo.org"'
 alias gdt='git difftool'
 alias gs='git status | cowsay -f bong -W 120 | lolcat -p 1'
 alias lh='ls -sh'
-alias codereview='vim ~/Dropbox/Raizlabs/code-review-checklist.md'
 alias startproject='django-admin.py startproject --template=~/projects/oss/django-twoscoops-project --extension=py,rst,html'
 alias play="open -a 'Xcode' ~/.scratchpad.playground"
-alias vpn="sudo openconnect -u john.watson --authgroup 2 https://bn-sslvpn.nuance.com"
+alias vpn="sudo openconnect --base-mtu 1400 -u john.watson --authgroup 2 --csd-wrapper ~/.cisco/csd-wrapper.sh https://bn-sslvpn.nuance.com"
 
-# Alias git to gh if available.
-if which gh &>/dev/null; then
-  eval "$(gh alias -s)"
-fi
+# Alias hub to git.
+eval "$(hub alias -s)"
--- tag-zsh/config/zsh/path.zsh
+++ tag-zsh/config/zsh/path.zsh
@@ -1,4 +1,6 @@
 path=( "/usr/bin" "/bin" "/usr/sbin" "/sbin" )
 path=( "$HOME/.fastlane/bin" "$HOME/.local/bin" "/usr/local/bin" "/usr/local/sbin" $path)
+path=( $(go env GOPATH)/bin $path)
+path=( "$HOME/.cargo/bin" $path)
 
-manpath=( "/usr/local/opt/erlang/lib/erlang/man" $manpath)
+export RUST_SRC_PATH="$HOME/.rustup/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src"
