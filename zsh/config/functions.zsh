b8d1345727ac3bc48f038feea468650dc81f2de7
John Watson <john@photodojo.org>
2018-10-22 22:33:57 -0400

    zsh: helpful Xcode-related functions.

---
+++ zsh/config/functions.zsh
@@ -0,0 +1,11 @@
+function eviscerateDerivedData
+{
+    local derivedDataPath="$HOME/Library/Developer/Xcode/DerivedData"
+    rm -rf "$derivedDataPath"
+    echo "Deleted $derivedDataPath"
+}
+
+function purgeZeroLengthFilesInDerivedData
+{
+  find ~/Library/Developer/Xcode/DerivedData -name "*.o" -size 0 | xargs rm
+}
